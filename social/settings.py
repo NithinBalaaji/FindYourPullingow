fe9b99cb5e938b80a4fbcef98c45a7519a038237
Nithin Balaaji <46069990+NithinBalaaji@users.noreply.github.com>
2020-04-21 13:22:42 +0530

    Update settings.py

--- social/settings.py
+++ social/settings.py
@@ -140,7 +140,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nithinbalaajidv@gmail.com'
-EMAIL_HOST_PASSWORD = 'Nittfest@1'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
 
 
